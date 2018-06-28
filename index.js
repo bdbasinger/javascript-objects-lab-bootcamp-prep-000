369f4e603e3db6749b8b24dcb7156214e243e357
Brennan B <bdbasinger777@gmail.com>
2018-06-28 21:41:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( {}, object, { [key]: value } )
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( object, { [key]: value } )
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object 
+}
+
+a = { b: "c" }
+
+updateObjectWithKeyAndValue(a, "d", "e")
+
+console.log(a)
